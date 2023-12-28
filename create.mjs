4eb3b60f85fa0c752981a41a83e5a776371d78c7
Santeri Hiltunen <git@me.hilzu.moe>
2023-12-28 13:40:17 +0200

    Update for modern node and latest dependencies

--- create.mjs
+++ create.mjs
@@ -1,26 +1,25 @@
 #!/usr/bin/env node
-"use strict";
 
-const chalk = require("chalk");
-const fs = require("fs");
-const path = require("path");
-const { promisify } = require("util");
+import chalk from "chalk";
+import {
+  copyFile,
+  mkdir,
+  readdir as readDir,
+  readFile,
+  stat,
+  writeFile,
+} from "node:fs/promises";
+import { basename as baseName, join as pathJoin } from "node:path";
+import { fileURLToPath } from "node:url";
 
-const copyFile = promisify(fs.copyFile);
-const mkdir = promisify(fs.mkdir);
-const readDir = promisify(fs.readdir);
-const readFile = promisify(fs.readFile);
-const stat = promisify(fs.stat);
-const writeFile = promisify(fs.writeFile);
-
+const dirname = fileURLToPath(new URL(".", import.meta.url));
 const explicitName = process.argv[2];
 const cwd = process.cwd();
-const projectName = explicitName || path.basename(cwd);
-const projectPath = explicitName ? path.join(cwd, projectName) : cwd;
-const templatePath = path.join(__dirname, "template");
-const packageManagerType = (process.env.npm_execpath || "").endsWith("yarn.js")
-  ? "yarn"
-  : "npm";
+const projectName = explicitName || baseName(cwd);
+const projectPath = explicitName ? pathJoin(cwd, projectName) : cwd;
+const templatePath = pathJoin(dirname, "template");
+const packageManagerType =
+  (process.env.npm_execpath || "").endsWith("yarn.js") ? "yarn" : "npm";
 const packageManagerRun = packageManagerType === "npm" ? "npm run" : "yarn";
 const packageManagerRunScript =
   packageManagerType === "npm" ? "npm run --" : "yarn run";
@@ -32,24 +31,22 @@ const log = (msg, ...args) => {
 const copyFiles = async () => {
   const templateFiles = await readDir(templatePath);
   for (const file of templateFiles) {
-    const templateFilePath = path.join(templatePath, file);
-    const projectFilePath = path.join(projectPath, file);
+    const templateFilePath = pathJoin(templatePath, file);
+    const projectFilePath = pathJoin(projectPath, file);
     const stats = await stat(templateFilePath);
 
     if (stats.isDirectory()) {
       await mkdir(projectFilePath);
       const newFiles = await readDir(templateFilePath);
-      templateFiles.push(...newFiles.map(f => path.join(file, f)));
+      templateFiles.push(...newFiles.map((f) => pathJoin(file, f)));
     } else if (stats.isFile()) {
       await copyFile(templateFilePath, projectFilePath);
     }
   }
 };
 
-const fromEntries = entries =>
-  entries.reduce((newObj, [key, value]) => ({ ...newObj, [key]: value }), {});
-
-const mapObject = (obj, mapper) => fromEntries(Object.entries(obj).map(mapper));
+const mapObject = (obj, mapper) =>
+  Object.fromEntries(Object.entries(obj).map(mapper));
 
 const create = async () => {
   log(`Creating project ${projectName}`);
@@ -63,12 +60,12 @@ const create = async () => {
 
   await copyFiles();
 
-  const packageJsonPath = path.join(projectPath, "package.json");
+  const packageJsonPath = pathJoin(projectPath, "package.json");
   const packageJsonFile = await readFile(packageJsonPath);
   const packageJson = JSON.parse(packageJsonFile);
 
   packageJson.name = projectName;
-  packageJson.engines.node = `^${process.versions.node}`;
+  packageJson.engines.node = `>=${process.versions.node}`;
   packageJson.scripts = mapObject(packageJson.scripts, ([key, value]) => [
     key,
     value.replace("PM_RUN", packageManagerRunScript),
@@ -83,26 +80,26 @@ Project created!
 You can now run "${packageManagerType} install" in the project directory to install the
 dependencies. Other useful scripts are:
 
-    ${chalk.keyword("gray")("# Build project")}
+    ${chalk.gray("# Build project")}
     ${packageManagerRun} build
-    ${chalk.keyword("gray")("# Clean all build artefacts")}
+    ${chalk.gray("# Clean all build artefacts")}
     ${packageManagerRun} clean
-    ${chalk.keyword("gray")("# Automatically format code using prettier")}
+    ${chalk.gray("# Automatically format code using prettier")}
     ${packageManagerRun} format
-    ${chalk.keyword("gray")("# Run project in production mode")}
+    ${chalk.gray("# Run project in production mode")}
     ${packageManagerRun} start
-    ${chalk.keyword("gray")(
+    ${chalk.gray(
       "# Run project in watch mode with automatic restarts on changes",
     )}
     ${packageManagerRun} dev
-    ${chalk.keyword("gray")("# Run eslint and check code formatting")}
+    ${chalk.gray("# Run eslint and check code formatting")}
     ${packageManagerRun} test
   `
     .trim()
     .split("\n")
-    .forEach(line => log(line));
+    .forEach((line) => log(line));
 };
 
-create().catch(err => {
+create().catch((err) => {
   console.error("create-ts-node: Failed to create project!", err);
 });
