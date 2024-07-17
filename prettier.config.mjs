e69cf7183e498352aefd57de189a311fd05bcabc
Santeri Hiltunen <git@me.hilzu.moe>
2024-07-17 12:08:26 +0300

    Use .mjs config for both prettier and eslint

---
+++ prettier.config.mjs
@@ -0,0 +1,9 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  experimentalTernaries: true,
+};
+
+export default config;
