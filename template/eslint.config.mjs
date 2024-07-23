25d80a2cddd0547a3817c5d70ba3265ec949eb7d
Santeri Hiltunen <git@me.hilzu.moe>
2024-07-23 10:15:59 +0300

    Add nodejs globals to eslint config

--- template/eslint.config.mjs
+++ template/eslint.config.mjs
@@ -1,10 +1,16 @@
 import js from "@eslint/js";
 import tsEslint from "typescript-eslint";
 import prettier from "eslint-config-prettier";
+import globals from "globals";
 
 export default tsEslint.config(
   {
     ignores: ["dist/", "node_modules/"],
+    languageOptions: {
+      globals: {
+        ...globals.nodeBuiltin,
+      },
+    },
   },
   js.configs.recommended,
   ...tsEslint.configs.recommended,
