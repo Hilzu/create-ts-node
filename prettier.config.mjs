6aa274bf41a16850cecfa174a92975343c93a562
Santeri Hiltunen <git@me.hilzu.moe>
2024-11-30 14:22:46 +0200

    Use simple json configuration for Prettier

--- prettier.config.mjs
+++
@@ -1,9 +0,0 @@
-/**
- * @see https://prettier.io/docs/en/configuration.html
- * @type {import("prettier").Config}
- */
-const config = {
-  experimentalTernaries: true,
-};
-
-export default config;
